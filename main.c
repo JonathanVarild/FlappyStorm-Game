1ddd89c80e036213d396e8b59aa1699d2c92bfa0
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-22 22:17:45 +0100

    Implemented test program

--- main.c
+++ main.c
@@ -1,5 +1,38 @@
 #include <pic32mx.h>
+#include "./screenengine.h"
+#include "./utilities.h"
 
-int main() {
+int main()
+{
+
+	display_init();
+
+	while (1)
+	{
+
+		int i;
+		for (i = 0; i < 118; i++)
+		{
+
+			matrix_reset();
+
+			draw_rect(10, 10, i, 20);
+			display_update();
+			tick_delay(100000);
+		}
+
+		for (i = 118; i >= 0; i--)
+		{
+
+			matrix_reset();
+
+			draw_rect(10, 10, i, 20);
+			display_update();
+			tick_delay(100000);
+		}
+	}
+
+	for (;;)
+		;
 	return 0;
 }
