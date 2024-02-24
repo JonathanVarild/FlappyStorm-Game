26cd417886d7d9656aedbb2912685c3b35d674ca
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-24 21:43:43 +0100

    Updated screen engine header

--- screenengine.h
+++ screenengine.h
@@ -1,6 +1,12 @@
+#include <pic32mx.h>
 #include <stdint.h>
+#include "./utilities.h"
+#include <stdbool.h>
 
-uint8_t spi_send_recv(uint8_t data);
+// Function prototypes
 void display_init(void);
 void display_update(void);
+void matrix_reset(void);
+void draw_pixel(int x, int y);
 void draw_rect(int x, int y, int width, int height);
+void draw_text(int x, int y, char *text, bool selected);
