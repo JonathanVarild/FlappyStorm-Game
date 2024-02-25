4c5c5bddc9e275dd33e68e7c107afd7cdc962505
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-25 19:15:11 +0100

    Updated main to test new functionality

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <pic32mx.h>
 #include "./gameengine.h"
 #include "./utilities.h"
-#include "./screenengine.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
 
 int main()
 {
+
+	static uint8_t smiley[] = { 0x00, 0x00, 0x48, 0x00, 0x48, 0x00, 0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x01, 0x02, 0x01, 0x01, 0xFE, 0x00, 0x00, 0x00, };
 
 	game_init();
 
@@ -15,7 +20,10 @@ int main()
 	struct entity* test_ent5 = create_entity((struct vector2D){100, 10}, 2, 2);
 	struct entity* test_ent6 = create_entity((struct vector2D){100, 10}, 3, 3);
 
-	struct label* test_lbl = create_label("Hello, world!", (struct vector2D){64, 10}, true, false);
+	set_entity_graphic(test_ent2, smiley);
+
+	struct label* test_lbl = create_label("Cent+Sel", (struct vector2D){64, 0}, true, true);
+	struct label* test_lbl2 = create_label("Normal", (struct vector2D){64, 10}, false, false);
 
 	while (1)
 	{
