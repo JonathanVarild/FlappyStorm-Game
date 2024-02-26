7dc067e395da15a64fd3979b84ab94cbed01013d
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-26 01:14:50 +0100

    Updated main to test input controls

--- main.c
+++ main.c
@@ -4,8 +4,28 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+struct entity* player;
+
+void jump()
+{
+	set_entity_velocity(player, (struct vector2D){0, -3});
+}
+
+void go_left()
+{
+	set_entity_velocity(player, (struct vector2D){-1, 0});
+}
+
+void go_right()
+{
+	set_entity_velocity(player, (struct vector2D){1, 0});
+}
+
 int main()
 {
+	button_4_down = jump;
+	button_3_down = go_left;
+	button_2_down = go_right;
 
 	static uint8_t smiley[] = { 0x00, 0x00, 0x48, 0x00, 0x48, 0x00, 0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 
   0x01, 0x02, 0x01, 0x01, 0xFE, 0x00, 0x00, 0x00, };
@@ -16,9 +36,9 @@ int main()
 
 	struct entity* reference_entity = create_entity((struct vector2D){45, 0}, 1, 1);
 
-	struct entity* test_ent = create_entity((struct vector2D){0, 20}, 10, 10);
-	set_entity_graphic(test_ent, smiley);
-	set_entity_velocity(test_ent, (struct vector2D){10, 0});
+	player = create_entity((struct vector2D){0, 20}, 10, 10);
+	set_entity_graphic(player, smiley);
+	set_entity_velocity(player, (struct vector2D){10, 0});
 
 	for (;;)
 		;
