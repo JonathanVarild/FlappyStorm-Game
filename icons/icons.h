49aedc26b5856eab87c4c1e8e78d3097fe2c5ced
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-26 14:10:26 +0100

    Added game XBM icons

---
+++ icons/icons.h
@@ -0,0 +1,22 @@
+#define icon_cloud_width 25
+#define icon_cloud_height 15
+
+#define icon_lightning_width 6
+#define icon_lightning_height 14
+
+#define icon_pipe_lower_width 11
+#define icon_pipe_lower_height 13
+
+#define icon_pipe_upper_width 11
+#define icon_pipe_upper_height 13
+
+#define icon_star_width 11
+#define icon_star_height 11
+
+static char icon_cloud[];
+static char icon_lightning[];
+static char icon_pipe_lower[];
+static char icon_pipe_upper[];
+static char icon_star[];
+
+
