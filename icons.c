a5f429f37b700c830ef12924d3b7c8af0271b6e6
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-29 12:22:38 +0100

    Cleaned up icons.c

--- icons.c
+++ icons.c
@@ -1,27 +1,3 @@
-#define icon_bird_width 10
-#define icon_bird_height 8
-
-#define icon_cloud_width 15
-#define icon_cloud_height 9
-
-#define icon_lightningcloud_width 15
-#define icon_lightningcloud_height 9
-
-#define icon_lightning_width 3
-#define icon_lightning_height 7
-
-#define icon_pipe_lower_width 11
-#define icon_pipe_lower_height 13
-
-#define icon_pipe_upper_width 11
-#define icon_pipe_upper_height 13
-
-#define icon_star_width 11
-#define icon_star_height 11
-
-#define icon_ground_width 256
-#define icon_ground_height 2
-
 char icon_bird[] = {
     0x7C,
     0x00,
