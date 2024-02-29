0ca150b4086f31ca277b41c7856f197a10696c2c
Jonathan Värild <Jonathan.varild@gmail.com>
2024-02-29 11:36:01 +0100

    Moved button polling code

--- gameengine.c
+++ gameengine.c
@@ -232,7 +232,8 @@ void set_label_text(struct label *lbl, char *text, bool centered)
         }
         lbl->x_offset = len / 2;
     }
-    else {
+    else
+    {
         lbl->x_offset = 0;
     }
 }
@@ -316,22 +317,18 @@ void game_init()
     // Set up clicky buttons as inputs.
     TRISDSET = 0x7F0;
 
-    // Enable interrupts for buttons.
-    IECSET(0) = 0x180000; // Enable interrupts for buttons.
-    IPCSET(4) = 0x1F000000; // Set priority level to 7 (1111 0000 0000 0000 0000 0000 0000 0000)
-
     // Set up Timer 2 interrupt with a period of 0.005 seconds. (200 fps / ticks per second)
-	T2CON = 0x0;			// Stop any existing timer2 and clear control register
-	T2CONSET = 0x70;		// Set prescaler to 1:256
-	PR2 = 3125;			    // Set period register to ((0.02 * 40000000) / 256) = 3125
-	TMR2 = 0x0;				// Clear the timer register
-	IFSCLR(0) = 0x100;		// Clear the Timer 2 interrupt flag
-	IECSET(0) = 0x100;		// Enable Timer 2 interrupts
-	T2CONSET = 0x8000;		// Start Timer 2
-	IPCSET(2) = 0x1A;		// Set priority level to 7 (0001 1100)
+    T2CON = 0x0;			// Stop any existing timer2 and clear control register
+    T2CONSET = 0x70;		// Set prescaler to 1:256
+    PR2 = 3125;			    // Set period register to ((0.02 * 40000000) / 256) = 3125
+    TMR2 = 0x0;				// Clear the timer register
+    IFSCLR(0) = 0x100;		// Clear the Timer 2 interrupt flag
+    IECSET(0) = 0x100;		// Enable Timer 2 interrupts
+    T2CONSET = 0x8000;		// Start Timer 2
+    IPCSET(2) = 0x1A;		// Set priority level to 7 (0001 1100)
 
     // Enable interrupts.
-	enable_interrupt();    
+    enable_interrupt();   
 }
 
 void game_tick()
@@ -402,7 +399,8 @@ void game_draw()
     int i;
     for (i = 0; i < DRAW_LOOP; i++)
     {
-        if (i < MAX_GAME_OBJECTS) {
+        if (i < MAX_GAME_OBJECTS)
+        {
             // Get the game object.
             struct game_object *obj = &game_objects[i];
 
@@ -447,7 +445,7 @@ void game_draw()
             // Check if the label is active.
             if (lbl->active)
             {
-                draw_text((int)(lbl->position.x) - lbl->x_offset, (int)(lbl->position.y), lbl->text,  lbl->selected);
+                draw_text((int)(lbl->position.x) - lbl->x_offset, (int)(lbl->position.y), lbl->text, lbl->selected);
             }
         }
     }
@@ -470,9 +468,12 @@ bool button_2_activated = false;
 void user_isr(void)
 {
     // Check if the Timer 2 interrupt flag is set.
-	if (IFS(0) & 0x100)
-	{   
-        if (!game_paused) {
+    if (IFS(0) & 0x100)
+    {
+
+        // Check if the game is paused.
+        if (!game_paused)
+        {
             // Increment the game uptime.
             game_uptime += 0.02;
 
@@ -480,42 +481,47 @@ void user_isr(void)
             game_tick();
             game_draw();
         }
+
+        // Get the button data.
+        int buttonData = ((PORTD & 0xE0) >> 5);
+        bool button_4_down = buttonData & 0b100;
+        bool button_3_down = buttonData & 0b010;
+        bool button_2_down = buttonData & 0b001;
+
+        // Check if clicky button 4 is pressed.
+        if (button_4_down && button_4_click != NULL && !button_4_activated)
+        {
+            button_4_click();
+            button_4_activated = true;
+        }
+        else if (!button_4_down && button_4_activated)
+        {
+            button_4_activated = false;
+        }
+
+        // Check if clicky button 3 is pressed.
+        if (button_3_down && button_3_click != NULL && !button_3_activated)
+        {
+            button_3_click();
+            button_3_activated = true;
+        }
+        else if (!button_3_down && button_3_activated)
+        {
+            button_3_activated = false;
+        }
+
+        // Check if clicky button 2 is pressed.
+        if (button_2_down && button_2_click != NULL && !button_2_activated)
+        {
+            button_2_click();
+            button_2_activated = true;
+        }
+        else if (!button_2_down && button_2_activated)
+        {
+            button_2_activated = false;
+        }
+
+        // Clear the Timer 2 interrupt flag.
         IFSCLR(0) = 0x100;
-	}
-
-    // Get the button data.
-    int buttonData = ((PORTD & 0xE0) >> 5);
-    bool button_4_down = buttonData & 0b100;
-    bool button_3_down = buttonData & 0b010;
-    bool button_2_down = buttonData & 0b001;
-
-    // Check if clicky button 4 is pressed.
-    if (button_4_down && button_4_click != NULL && !button_4_activated)
-    {
-        button_4_click();
-        button_4_activated = true;
-    }
-    else if(!button_4_down && button_4_activated) {
-        button_4_activated = false;
-    }
-
-    // Check if clicky button 3 is pressed.
-    if (button_3_down && button_3_click != NULL && !button_3_activated)
-    {
-        button_3_click();
-        button_3_activated = true;
-    }
-    else if(!button_3_down && button_3_activated) {
-        button_3_activated = false;
-    }
-
-    // Check if clicky button 2 is pressed.
-    if (button_2_down && button_2_click != NULL && !button_2_activated)
-    {
-        button_2_click();
-        button_2_activated = true;
-    }
-    else if(!button_2_down && button_2_activated) {
-        button_2_activated = false;
     }
 }
